9161e484c8b83d27477cdb49068f1c797864a0a2
Shanmugavel <shanmugavel.k3@tcs.com>
2026-03-09 23:27:43 +0530

    Initial commit: Tech Quiz Application with skip timer feature

---
+++ src/main/java/com/shank/Tech/quiz/TechQuizApplication.java
@@ -0,0 +1,13 @@
+package com.shank.Tech.quiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechQuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechQuizApplication.class, args);
+	}
+
+}
